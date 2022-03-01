6fc4baa27264df8e94c7b920f999d2f2d45b93ff
Giovanni Gibelli <giogibelli@icloud.com>
2022-03-01 13:56:30 +0100

    new setup

--- setup.py
+++ setup.py
@@ -1,15 +1,14 @@
 from setuptools import setup, find_packages
 
-
 setup(
     name="mypy_junitizer",
     version="0.0.1",
-    url="http://www.cmsoftware.se",
+    url="https://www.cmsoftware.se",
     author="CM Software AB",
     author_email="development@cmsoftware.se",
     description="Script for converting output from MyPy to Junit XML format",
     packages=find_packages(),
     python_requires=">=3.9",
     entry_points={"console_scripts": ["mypy_junitizer = mypy_junitizer.mypy_junitizer:main"]},
-    install_requires=['junit-xml']
+    install_requires=['junit-xml', 'mypy', 'pydantic', 'sqlalchemy-stubs']
 )
